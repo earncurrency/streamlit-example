084aa26770d79ddbe41144c79cae06c64f28e9cd
earncurrency <earncurrency@gmail.com>
2022-11-24 11:36:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ st.write("""
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-  #df = pd.read_csv(uploaded_file)
-  df = pd.read_excel(uploaded_file)
+  df = pd.read_csv(uploaded_file)
+  #df = pd.read_excel(uploaded_file)
   #df = pd.DataFrame()
   #st.write(df)
   st.write("""Success""")
