141b62c115ca166882a9ce056ee500d25db28419
earncurrency <earncurrency@gmail.com>
2022-11-24 12:49:04 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,14 +13,4 @@ if uploaded_file is not None:
   st.write(df)
   
   fig, ax = plt.subplots()
-  df.hist(
-    bins=8,
-    column="Age",
-    grid=False,
-    figsize=(8, 8),
-    color="#86bf91",
-    zorder=2,
-    rwidth=0.9,
-    ax=ax,
-  )
-  st.write(fig)
+ 
