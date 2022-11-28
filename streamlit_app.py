b400fe90e83aecd8481a270f3cf80f7fbeb2d4d5
earncurrency <earncurrency@gmail.com>
2022-11-28 16:15:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+from st_aggrid import AgGrid
 import pandas as pd
 import streamlit as st
 
@@ -9,4 +10,5 @@ st.write("""
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
-  st.write(df)
+  # st.write(df)
+  AgGrid(df)
