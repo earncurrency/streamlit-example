17137b54a81cae4a579872b182b800bf723ff25a
earncurrency <earncurrency@gmail.com>
2022-11-29 13:40:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import pandas as pd
 from st_aggrid import AgGrid
+import mitosheet
 
 st.write("""
 # Test
 ## test test
 """)
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  st.write(df)
-  #AgGrid(df)
+mitosheet.sheet()
+
+# uploaded_file = st.file_uploader("Choose a file")
+# if uploaded_file is not None:
+#   df = pd.read_csv(uploaded_file)
+#   st.write(df)
+#   #AgGrid(df)
