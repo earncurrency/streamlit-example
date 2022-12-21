09ab707f023ed91e96c065a63494f3bd1c25cbb1
earncurrency <earncurrency@gmail.com>
2022-12-21 09:41:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,7 @@ st.write("""
 ## test test
 """)
 
-with st.sidebar:
-  uploaded_file = st.file_uploader("Choose a file")
+uploaded_file = st.file_uploader("Choose a file")
   
 if uploaded_file is not None:
   df = pd.read_csv(uploaded_file)
