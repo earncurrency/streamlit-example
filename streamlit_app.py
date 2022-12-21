67d22396eed0a6670c467e67203d7c8f3961b0fc
earncurrency <earncurrency@gmail.com>
2022-12-21 09:43:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,10 @@ st.write("""
 ## test test
 """)
 
-uploaded_file = st.file_uploader("Choose a file")
+with st.sidebar:
+  uploaded_file = st.file_uploader("Choose a file")
   
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file)
-  #st.write(df)
-  AgGrid(df)
+  if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    #st.write(df)
+    AgGrid(df)
